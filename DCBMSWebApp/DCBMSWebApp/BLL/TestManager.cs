b4d9e298743d5a1b0493291a152017b2b3b5ca46
tahmina <tahmina.islam56@gmail.com>
2017-05-13 20:31:50 +0600

    <add> TestManager

---
+++ DCBMSWebApp/DCBMSWebApp/BLL/TestManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DCBMSWebApp.DAL.Gateway;
+using DCBMSWebApp.Models;
+
+namespace DCBMSWebApp.BLL
+{
+    public class TestManager
+    {
+        TestGateway _testGateway = new TestGateway();
+        public string Save(Test test)
+        {
+            if (_testGateway.IsTestExist(test))
+            {
+                return "Test already exists.";
+            }
+            int rowAffected = _testGateway.Save(test);
+            if (rowAffected > 0)
+            {
+                return "Test saved successfully.";
+            }
+            return "Failed to save test.";
+        }
+
+        public List<Test> GetAll()
+        {
+            return _testGateway.GetAll();
+        }
+    }
+}
