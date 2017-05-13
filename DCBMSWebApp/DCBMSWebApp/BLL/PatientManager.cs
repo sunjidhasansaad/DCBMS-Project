e9ca7ceb79f59474ff0f3131579a3c85beb2fd33
tahmina <tahmina.islam56@gmail.com>
2017-05-13 21:49:55 +0600

    <add> PatientManager

---
+++ DCBMSWebApp/DCBMSWebApp/BLL/PatientManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DCBMSWebApp.DAL.Gateway;
+using DCBMSWebApp.Models;
+
+namespace DCBMSWebApp.BLL
+{
+    public class PatientManager
+    {
+        PatientGateway _patientGateway = new PatientGateway();
+        public string Save(Patient aPatient)
+        {
+            int rowAffected = _patientGateway.Save(aPatient);
+
+            if (rowAffected > 0)
+            {
+                return "Patient Saved Successfully.";
+
+            }
+            return "Failed to save Patient !!";
+
+        }
+    }
+}
