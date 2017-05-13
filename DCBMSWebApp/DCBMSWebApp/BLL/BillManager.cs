7d97eb5cc42d60927e63fb854c784758b5f9eb0b
tahmina <tahmina.islam56@gmail.com>
2017-05-13 22:04:31 +0600

    <add> BillManager

---
+++ DCBMSWebApp/DCBMSWebApp/BLL/BillManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DCBMSWebApp.DAL.Gateway;
+using DCBMSWebApp.Models;
+
+namespace DCBMSWebApp.BLL
+{
+    public class BillManager
+    {
+        BillGateway _billGateway = new BillGateway();
+        public string Save(Bill bill)
+        {
+            if (!_billGateway.IsBillNoExist(bill))
+            {
+                int rowAffected = _billGateway.Save(bill);
+
+                if (rowAffected > 0)
+                {
+                    return "Bill Saved Succesfully.";
+                }
+
+            }
+            return "Failed to save bill !!";
+        }
+
+        public bool IsBillNoExist(Bill bill)
+        {
+            return _billGateway.IsBillNoExist(bill);
+        }
+    }
+}
