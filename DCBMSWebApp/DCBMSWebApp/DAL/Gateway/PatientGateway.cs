36db4f9805db99f77fa1ab930994a47d6327768c
tahmina <tahmina.islam56@gmail.com>
2017-05-13 21:46:07 +0600

    <add> PatientGateway

---
+++ DCBMSWebApp/DCBMSWebApp/DAL/Gateway/PatientGateway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using DCBMSWebApp.Models;
+
+namespace DCBMSWebApp.DAL.Gateway
+{
+    public class PatientGateway
+    {
+        private string connectionString =
+          WebConfigurationManager.ConnectionStrings["DCBMSConnectionString"].ConnectionString;
+        public int Save(Patient aPatient)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = @"INSERT INTO Patients(Name,DateOfBirth,MobileNo,BillNo) VALUES('"
+                + aPatient.Name + "','" + aPatient.DateOfBirth + "','" + aPatient.MobileNo + "','" + aPatient.BillNo + "')";
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            connection.Open();
+            int rowAffected = command.ExecuteNonQuery();
+            connection.Close();
+            return rowAffected;
+        }
+    }
+}
