bbee6fb77f25fe1f88d90ff0bf4e9cb56b0134ce
tahmina <tahmina.islam56@gmail.com>
2017-05-13 22:09:40 +0600

    <add> TestBillManager

---
+++ DCBMSWebApp/DCBMSWebApp/BLL/TestBillManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DCBMSWebApp.DAL.Gateway;
+using DCBMSWebApp.DAL.Models;
+
+namespace DCBMSWebApp.BLL
+{
+    public class TestBillManager
+    {
+      
+        TestBillGateway _testBillGateway= new TestBillGateway();
+        public void Save(TestBillVM testBillVm)
+        {
+            int rowAffected =  _testBillGateway.Save(testBillVm);
+
+        }
+    }
+}
