7eb821941ab182cf7dfe4577d7c9415e1aa4551a
tahmina <tahmina.islam56@gmail.com>
2017-05-20 21:56:43 +0600

    <add> TypeWiseReport

---
+++ DCBMSWebApp/DCBMSWebApp/DAL/Models/TypeWiseReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCBMSWebApp.DAL.Models
+{
+    public class TypeWiseReport
+    {
+        public string TypeName { get; set; }
+        public string TotalNoOfTest { get; set; }
+        public string TotalAmount { get; set; }
+    }
+}
